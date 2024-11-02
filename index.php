74f239bdb9320a219e183e17b50ecd0c75b9956c
Bhanuka99 <bhanuka.ariyarathne@gmail.com>
2024-11-02 11:44:54 +0530

    initial commit

---
+++ index.php
@@ -0,0 +1,23 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+    <link rel="stylesheet" type="text/css" href="bootstrap/css/bootstrap.css"/>
+</head>
+<body>
+    <div class ="container-fluid">
+        <div class ="col-md-6 col-sm-8" style ="height:50px; background-color:red;">
+
+        </div>
+        <div class ="col-md-6 col-sm-4" style ="height:50px; background-color:blue;">
+
+        </div>
+        <div class = "row"></div>
+        <div class ="col-md-6 col-sm-4" style ="height:50px; background-color:blue;">
+
+        </div>
+    </div>
+</body>
+</html>
---
+++ view/login.php
@@ -0,0 +1,16 @@
+<html>
+<head>
+    <title>login</title>
+    <link rel="stylesheet" type="text/css" href="../bootstrap/css/bootstrap.css"/>
+</head>
+<body>
+    <div clss="container">
+        <div class="row" style="height:100px"></div>
+        <div class="row">
+            <div class="col-md-8 col-md-offset-2" style="background-color:green; height:300px">
+                &nbsp;
+            </div>
+        </div> 
+    </div>
+</body>
+</html>
