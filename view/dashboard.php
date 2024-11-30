7c37c758cc41005cfe9008ac13dbaeeb05069f04
Bhanuka99 <bhanuka.ariyarathne@gmail.com>
2024-11-30 11:35:09 +0530

    2024-11-30

--- view/dashboard.php
+++ view/dashboard.php
@@ -1,3 +1,10 @@
+<?php 
+include_once '../commons/session.php';
+
+// to get the user
+$userrow=$_SESSION["user"];
+?>
+
 <html>
 <head>
     <title>Dashboard</title>
@@ -5,17 +12,28 @@
 </head>
 <body>
     <div clss="container">
-        <!-- <div class="row" style="height:100px"></div> -->
-        <div class="col-md-3">
-            <img src="../images/logo.png" width="80px" height="100px"/>
+        <div class="row" style="height: 40px;">&nbsp;</div>
+        <div class="row" style="height:75px">
+        <div class="col-md-2 col-md-offset-1" >
+            <img src="../images/logo.png" width="70px" height="80px"/>
         </div>
         <div class="col-md-6">
-            Director
+            <h1 align ="center">Online Clothestore</h1>
         </div>
-        <div class="col-md-3">
+        </div>
+        <hr>
+        <div class="row">
+        <div class="col-md-2 col-md-offset-1" >
+            <?php echo ucwords($userrow["user_fname"]." ".$userrow["user_lname"]) ?>
+        </div>
+        <div class="col-md-6">
+        <h4 align ="center">Online Clothestore</h4>
+        </div>
+        <div class="col-md-2 col-md-offset-1">
             <a href="" class="btn btn-primary">Log out</a>
-        </div> 
-    </div>
+        </div>  
+        </div>    
+    </div>    
 </body>
 <script src="../js/jquery-3.7.1.js"></script>
 <!-- <script src="../js/loginvalidation.js"></script> -->
